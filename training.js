6852d8a7d13b8c4079100df20456f6513f4430e3
Evgenia <evgenia-m@yandex.ru>
2022-06-24 00:02:09 +0200

    до 35 урока включительно

--- training.js
+++ training.js
@@ -206,3 +206,88 @@ console.log(tea, burger);
 
 let {tea, juice, milk} = menu.drinks;
 console.log(tea, juice, milk);
+
+const arr = [2, 3, 6, 8];
+/* arr.pop();
+
+arr.push(10); */
+
+arr.forEach(function(item, i, arr){
+    console.log(`${i}: ${item} в массиве ${arr}`);
+
+});
+
+const a = 'Я хочу наконец спать';
+const b = a.split(' ');
+/* console.log(b); */
+console.log(b.join(' '));
+
+/* const obj = {
+    a:5,
+    b:1
+};
+
+const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj); */
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+function copy(mainObj) {
+    let objCopy = {};
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const newNumbers = copy(numbers);
+
+const add = {
+    a: 10,
+    b: 15
+};
+
+const clone = Object.assign({}, add);
+clone.a = 20;
+console.log(add);
+console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'hallo';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['a', 'b', 'c'],
+      blogs = ['d', 'e', 'f'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+      console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [1, 2, 3];
+log(...num);
+
+const object = {
+    a: 1,
+    b: 2
+};
+
+const copyObject = {...object};
+console.log(copyObject);
