09f0881767940bdf65bc92165bcad18642b73f85
Evgenia <evgenia-m@yandex.ru>
2022-07-11 17:40:48 +0200

    до упражнения после 37 урока включтельно

---
+++ trainings2.js
@@ -0,0 +1,93 @@
+'use strict';
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000,
+    
+};
+/* let eachShopArea = [];
+function shopArea(obj) {
+    
+    shoppingMallData.shops.forEach(function(item, i) {
+        eachShopArea[i] = item.width * item.length;
+    });
+    console.log(eachShopArea);
+}
+
+shopArea(shoppingMallData);
+
+let shopVolume = [];
+eachShopArea.forEach(function(item, i) {
+    shopVolume[i] = item*shoppingMallData.height;
+});
+
+console.log(shopVolume);
+let budget = [];
+
+shopVolume.forEach((element, i, shopVolume) => { 
+    budget[i] = element * shoppingMallData.moneyPer1m3;
+ });
+ console.log(budget);
+
+let sum = 0;
+
+budget.forEach((element, i) => {
+    sum += element;
+});
+
+console.log(sum);
+
+
+let shopFirst = {...shoppingMallData.shops[0]};
+console.log(shopFirst); */
+
+
+function isBudgetEnough(data) {
+    let eachShopArea = [];
+    data.shops.forEach(function(item, i) {
+        eachShopArea[i] = item.width * item.length;
+    });
+    let shopVolume = [];
+
+    eachShopArea.forEach(function(item, i) {
+        shopVolume[i] = item*data.height;
+    });
+
+    let budget = [];
+    
+    shopVolume.forEach((element, i, shopVolume) => { 
+        budget[i] = element * data.moneyPer1m3;
+     });
+
+     let sum = 0;
+
+    budget.forEach((element, i) => {
+        sum += element;
+    });
+
+    if (data.budget - sum >= 0) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
+    }
+}
+
+console.log(isBudgetEnough(shoppingMallData));
